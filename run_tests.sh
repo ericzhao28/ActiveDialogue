4e12339a48a9bf579b4f868344c18c984a8f9a5f
Eric Zhao <ericlzhao28@gmail.com>
2020-02-09 15:54:03 +0000

    Update tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,6 @@
-python3 -m tests.dst_env
+python3 -m tests.trivial_train --model gce --lr 0.1 --epochs 3 --seed 30300 --al_batch 4 --pool_size 30 --label_budget 9 --fit_items 16 --seed_size 64 --seed_epochs 2 --batch_size 2
+python3 -m tests.trivial_train --model glad --lr 0.1 --epochs 3 --seed 30301 --al_batch 4 --pool_size 15  --label_budget 9 --fit_items 16 --seed_size 64 --seed_epochs 2 --batch_size 2
+python3 -m tests.random_dst_env --model glad --lr 0.1 --epochs 3 --seed 30301 --al_batch 4 --pool_size 15  --label_budget 9 --fit_items 16 --seed_size 64 --seed_epochs 2 --batch_size 2 --device 0
+python3 -m tests.dst_env --lr 0.1 --epochs 3 --seed 30301 --al_batch 4 --pool_size 15  --label_budget 9 --fit_items 16 --seed_size 64 --seed_epochs 2 --batch_size 2 --device 0
 python3 -m tests.naive_baselines
 python3 -m tests.dataset
-python3 -m tests.trivial_train --model gce --lr 0.1 --epochs 3
-python3 -m tests.trivial_train --model glad --lr 0.1 --epochs 3
--- tests/trivial_train.py
+++ tests/trivial_train.py
@@ -1,6 +1,5 @@
-import argparse
 import logging
-import os
+from ActiveDialogue.main.utils import get_args
 from ActiveDialogue.datasets.woz.wrapper import load_dataset
 from ActiveDialogue.models.glad import GLAD
 from ActiveDialogue.models.gce import GCE
@@ -8,49 +7,7 @@ from pprint import pprint
 
 
 def main():
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument("--seed", type=int, default=239342)
-    parser.add_argument('--device', type=int, default=None)
-    parser.add_argument('--model', type=str, default='glad')
-    parser.add_argument('--epochs', type=int, default=1)
-    parser.add_argument('--seed_epochs', type=int, default=1)
-    parser.add_argument('--batch_size', type=int, default=50)
-    parser.add_argument('--dexp',
-                        help='root experiment folder',
-                        default='exp')
-    parser.add_argument('--demb',
-                        help='word embedding size',
-                        default=400,
-                        type=int)
-    parser.add_argument('--dhid',
-                        help='hidden state size',
-                        default=200,
-                        type=int)
-    parser.add_argument('--lr',
-                        help='learning rate',
-                        default=1e-3,
-                        type=float)
-    parser.add_argument('--stop',
-                        help='slot to early stop on',
-                        default='joint_goal')
-    parser.add_argument('--resume', help='save directory to resume from')
-    parser.add_argument('-n',
-                        '--nick',
-                        help='nickname for model',
-                        default='default')
-    parser.add_argument('--dropout',
-                        nargs='*',
-                        help='dropout rates',
-                        default=['emb=0.2', 'local=0.2', 'global=0.2'])
-
-    args = parser.parse_args()
-    args.dout = os.path.join(args.dexp, args.model, args.nick)
-    args.dropout = {
-        d.split('=')[0]: float(d.split('=')[1]) for d in args.dropout
-    }
-    if not os.path.isdir(args.dout):
-        os.makedirs(args.dout)
+    args = get_args()
 
     datasets, ontology, vocab, E = load_dataset()
     ptrs, seed_ptrs, num_turns = datasets["train"].get_turn_ptrs(
