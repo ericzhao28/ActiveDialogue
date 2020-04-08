4c9f255721146c3a5e70f67633ae2a0e1cc7bc16
Eric Zhao <ericlzhao28@gmail.com>
2020-04-08 20:55:02 +0000

    Remove scaffolding

--- ActiveDialogue/strategies/common.py
+++ ActiveDialogue/strategies/common.py
@@ -18,7 +18,6 @@ class ThresholdStrategy():
     def observe(self, obs):
         aobs, legend = split(obs)
         value = self._measure_uncertainty(aobs)
-        print(value)
         value = value > self.threshold
         value = np.array(value, dtype=np.int32)
         if self._need_unsplit:
